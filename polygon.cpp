d8da27a5e8df3e73ab76ed6ee402de974b1954e6
Tamaghno Chaudhuri <tamaghno.chaudhuri@gmail.com>
2019-10-17 20:32:10 +0530

    Add files via upload

--- polygon.cpp
+++ polygon.cpp
@@ -1,3 +1,8 @@
+//Class polygon contains data member width and height and public method set_value() toassign values to width and height.
+//Class Rectangle and Triangle are inherited from polygon class. Both the classes contain public method calculate_area() to calculate the area of Rectangle and Triangle.
+//Use base class pointer to access the derived class object and show the area calculated.
+
+
 #include<iostream>
 using namespace std;
 
